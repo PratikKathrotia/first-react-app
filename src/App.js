440953d62d0947d56b64adf33c6c1d16f5ccfae6
Pratik Kathrotia <pratik@Pratiks-MacBook-Air.local>
2020-12-01 15:53:31 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>This is my first react app</p>
+      </div>
+    );
+  }
+}
+
+export default App;
